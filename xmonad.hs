b2ec6056eee8f8db0fc5e79b4824642c5a8ab1fc
bag-man <garland.owen@gmail.com>
2015-02-07 21:58:16 +0000

    Updated xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -3,7 +3,9 @@ import XMonad.Actions.CycleWS
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.ManageHelpers
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
 
 main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
 
@@ -21,17 +23,23 @@ myPP = xmobarPP {
 }
 
 myKeys = [ 
-  ((mod1Mask, xK_w), spawn "firefox"),
-  --((mod1Mask, xK_q), spawn "urxvt"),
-  ((0, xK_F10), spawn "ncmpcpp toggle"),
+  ((mod4Mask, xK_w), spawn "firefox"),
+  ((mod4Mask, xK_q), spawn "urxvt"),
   ((0, xK_F9), spawn "ncmpcpp next"),
+  ((0, xK_F10), spawn "ncmpcpp toggle"),
+  ((0, xK_F11),spawn "amixer set Master 5-"),
+  ((0, xK_F12),spawn "amixer set Master 5+"),
+  ((mod1Mask, xK_F4), kill),
   ((controlMask .|. mod1Mask, xK_Right), nextWS),
   ((controlMask .|. mod1Mask, xK_Left), prevWS),
   ((mod1Mask .|. shiftMask, xK_Right), shiftToNext >> nextWS),
-  ((mod1Mask .|. shiftMask, xK_Left),   shiftToPrev >> prevWS)
+  ((mod1Mask .|. shiftMask, xK_Left), shiftToPrev >> prevWS),
+  ((mod1Mask .|. shiftMask, xK_period), sendMessage(IncMasterN(-1))) -- Shift+Alt+. 
  ]
 
 toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
+
+myManageHook = composeAll[isFullscreen --> (doF W.focusDown <+> doFullFloat)]
 
 myConfig = defaultConfig {
   terminal           = myTerminal,
@@ -39,5 +47,7 @@ myConfig = defaultConfig {
   normalBorderColor  = myBorderColour,
   focusedBorderColor = myBorderColour,
   workspaces         = myWorkspaces,
+  manageHook         = myManageHook,
   layoutHook         = smartBorders $ layoutHook defaultConfig
-} `additionalKeys` myKeys
+} `removeKeys` [(mod1Mask, xK_period)] -- Removes Alt+.
+  `additionalKeys` myKeys 

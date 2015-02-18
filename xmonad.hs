d5e2b244c624295e92129eb615619a6ff12c72ad
Owen Garland <garland.owen@gmail.com>
2015-02-18 19:18:17 +0000

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -26,6 +26,8 @@ myKeys = [
   -- Chromebook
   ((mod1Mask, xK_w), spawn "firefox"),
   ((mod1Mask, xK_q), spawn "urxvt"),
+  ((0, xK_F6), spawn "xbacklight -dec 10"),
+  ((0, xK_F7), spawn "xbacklight -inc 10"),
   ((0, xK_F9), spawn "touchtoggle"),
   ((0, xK_Super_L), spawn "dmenu_run"),
 

0a43a2b81c88d04c85ff77ac2b2b4539d3da7b12
Owen Garland <owen.garland@clock.co.uk>
2016-09-12 11:29:02 +0100

    Added screenshot whole screen

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -45,7 +45,8 @@ myKeys = [
   ((mod1Mask, xK_w), spawn "google-chrome-stable"),
   ((mod1Mask, xK_F4), kill),
   ((mod1Mask, xK_End), spawn "vlock -ans"),
-  ((0, xK_Print), spawn "sleep 0.2; scrot -s /tmp/shot.png; copyq write image/png - < /tmp/shot.png && copyq select 0"),
+  ((0, xK_Print), spawn "sleep 0.2; scrot -q 100 -s /tmp/shot.png; copyq write image/png - < /tmp/shot.png && copyq select 0"),
+  ((controlMask, xK_Print), spawn "scrot -q 100 /tmp/shot.png; copyq write image/png - < /tmp/shot.png && copyq select 0"),
   
   -- Adjust split
   ((mod1Mask, xK_j), sendMessage MirrorShrink),

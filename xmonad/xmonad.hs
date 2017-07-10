420bf5b98353444a93b4b173e764269cb3c001f5
Owen Garland <owen.garland@clock.co.uk>
2017-07-10 20:55:14 +0200

    Updated to rofi

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -33,7 +33,6 @@ myKeys = [
   -- Chromebook
   ((0, xK_F6), spawn "xbacklight -dec 10"),
   ((0, xK_F7), spawn "xbacklight -inc 10"),
-  ((0, xK_Super_L), spawn "dmenu_run"),
 
   -- Audio
   ((0, xK_F9), spawn "mpc next"),
@@ -45,6 +44,7 @@ myKeys = [
   ((mod1Mask, xK_w), spawn "google-chrome-stable"),
   ((mod1Mask, xK_F4), kill),
   ((mod1Mask, xK_End), spawn "vlock -ans"),
+  ((mod1Mask, xK_p), spawn "rofi -show run"),
   ((0, xK_Print), spawn "sleep 0.2; scrot -q 100 -s /tmp/shot.png; copyq write image/png - < /tmp/shot.png && copyq select 0"),
   ((controlMask, xK_Print), spawn "scrot -q 100 /tmp/shot.png; copyq write image/png - < /tmp/shot.png && copyq select 0"),
   
